c4de7c28cde64aa59f3160a3c0486903c196e40d
TheEdster3 <TheEdster3@gmail.com>
2018-06-10 15:36:09 -0700

    Added fibonacci algorithms

---
+++ C#/Fibonacci_Dynamic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Fibonacci_Dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] fib = Fibonacci_Dynamic(20);
+            for(int i = 0; i < fib.Length - 2; i++)
+            {
+                Console.Write(fib[i] + " ");
+            }
+        }
+
+        static int[] Fibonacci_Dynamic(int n)
+        {
+            if(n == 0)
+                return new int[] {0};
+            int[] fib = new int[n + 2]; //Prevents out of bounds with a small arrays
+            fib[0] = 0;
+            fib[1] = 1;
+
+            for(int i = 2; i < n; i++)
+            {
+                fib[i] = fib[i - 1] + fib[i - 2];
+            }
+
+            return fib;
+        }
+    }
+}
---
+++ C#/Fibonacci_Recursive/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Fibonacci_Recursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintFibonacci(20);
+        }
+        
+        static void PrintFibonacci(int n)
+        {
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write(Fibonacci_Recursive(i) + " ");
+            }
+        }
+        static int Fibonacci_Recursive(int n)
+        {
+            if(n == 0)
+                return 0;
+            if(n == 1)
+                return 1;
+            int tempNumber = Fibonacci_Recursive(n - 2) + Fibonacci_Recursive(n - 1);
+            return tempNumber;
+        }
+    }
+}
