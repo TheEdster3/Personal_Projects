3f4697e2181100eb9cc54a9d66a3b2ccf630a2c1
TheEdster3 <TheEdster3@gmail.com>
2018-06-10 16:16:55 -0700

    Added some string algorithms

---
+++ C#/String_Algorithms/Is_Palindrome/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Is_Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World: " + Is_Palindrome("Hello World"));
+            Console.WriteLine("Watermelon: " + Is_Palindrome("Watermelon"));
+            Console.WriteLine("racecar: " + Is_Palindrome("racecar"));
+            Console.WriteLine("ToToT: " + Is_Palindrome("ToToT"));
+        }
+
+        static bool Is_Palindrome(string userInput)
+        {
+            for(int i = 0, j = userInput.Length - 1; i < j; i++, j--)
+            {
+                if(userInput[i] != userInput[j])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ C#/String_Algorithms/Reverse_String/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World: " + Reverse_String("Hello World"));
+            Console.WriteLine("Watermelon: " + Reverse_String("Watermelon"));
+            Console.WriteLine("racecar: " + Reverse_String("racecar"));
+            Console.WriteLine("Reverse this string: " + Reverse_String("Reverse this string"));
+            Console.Write("This was reversed recursively: ");
+            Console.WriteLine(Reverse_String_Recursive("This was reversed recursively"));
+
+        }
+
+        static string Reverse_String(string userInput)
+        {
+            string reversedString = "";
+            for(int i = userInput.Length - 1; i >= 0; i--)
+            {
+                reversedString += userInput[i];
+            }
+            return reversedString;
+        }
+
+        static string Reverse_String_Recursive(string userInput)
+        {
+            if(userInput.Length == 1)
+                return userInput[0].ToString();
+            
+            Console.Write(userInput[userInput.Length - 1]);
+            return Reverse_String_Recursive(userInput.Substring(0, userInput.Length - 1));
+        }
+    }
+}
